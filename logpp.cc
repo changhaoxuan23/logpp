de1e6115ffaef6869c1de9abe39d610747e218df
changhaoxuan23 <151429809+changhaoxuan23@users.noreply.github.com>
2024-06-26 19:12:01 +0800

    init

---
+++ logpp.cc
@@ -0,0 +1,82 @@
+#include <logpp.hh>
+#include <print>
+#include <ranges>
+#include <unordered_map>
+namespace {
+static std::unordered_map<LogPP::LogLevel, const char *> style_map{
+  {LogPP::LogLevel::trace, "\x1b[2;37m"},
+  {LogPP::LogLevel::debug, "\x1b[37m"},
+  {LogPP::LogLevel::information, "\x1b[36m"},
+  {LogPP::LogLevel::warning, "\x1b[33m"},
+  {LogPP::LogLevel::error, "\x1b[1;35m"},
+  {LogPP::LogLevel::critical, "\x1b[1;4;5;31m"},
+};
+static std::unordered_map<LogPP::LogLevel, char> tag_map{
+  {LogPP::LogLevel::trace, 'T'},
+  {LogPP::LogLevel::debug, 'D'},
+  {LogPP::LogLevel::information, 'I'},
+  {LogPP::LogLevel::warning, 'W'},
+  {LogPP::LogLevel::error, 'E'},
+  {LogPP::LogLevel::critical, 'C'},
+};
+static void
+write_log(LogPP::LogLevel level, std::vector<std::string> &chain, std::string &&time, std::string &&content) {
+  std::string prefix = style_map[level];
+  prefix.push_back('[');
+  prefix.push_back(tag_map[level]);
+  prefix.push_back(']');
+  prefix.append(time);
+  if (!chain.empty()) {
+    bool start = true;
+    prefix.push_back('[');
+    for (const auto &name : std::ranges::reverse_view(chain)) {
+      if (start) {
+        start = false;
+      } else {
+        prefix.append("/");
+      }
+      prefix.append(name);
+    }
+    prefix.push_back(']');
+  }
+  prefix.push_back(' ');
+  std::string suffix = "\x1b[0m";
+  size_t      start  = 0;
+  while (true) {
+    auto find = content.find('\n', start);
+    if (find == content.npos) {
+      if (start < content.size()) {
+        std::println("{}{}{}", prefix, content.substr(start), suffix);
+      }
+      break;
+    }
+    std::println("{}{}{}", prefix, content.substr(start, find - start), suffix);
+    start = find + 1;
+  }
+}
+}; // namespace
+void LogPP::Logger::do_log(
+  LogLevel level, std::vector<std::string> &chain, std::string &&time, std::string &&content
+) const {
+  if (level < this->log_level) {
+    return;
+  }
+  if (this->parent_logger == nullptr) {
+    return write_log(level, chain, std::move(time), std::move(content));
+  }
+  chain.emplace_back(this->name);
+  this->parent_logger->do_log(level, chain, std::move(time), std::move(content));
+}
+LogPP::Logger::Logger() : log_level(LogLevel::information), parent_logger(nullptr), name("") {}
+LogPP::Logger::Logger(const Logger *const parent, std::string_view name)
+  : log_level(parent->log_level), parent_logger(parent), name(name) {}
+
+LogPP::Logger &LogPP::Logger::get_global_logger() {
+  static Logger logger{};
+  return logger;
+}
+
+[[nodiscard]] LogPP::Logger LogPP::Logger::create_sub_logger(std::string_view name) const {
+  return {this, name};
+}
+void LogPP::Logger::set_log_level(LogLevel new_level) { this->log_level = new_level; }
---
+++ logpp.hh
@@ -0,0 +1,68 @@
+#ifndef LOGPP_HH_
+#define LOGPP_HH_
+#include <chrono>
+#include <cstdint>
+#include <format>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <vector>
+namespace LogPP {
+enum LogLevel : uint8_t {
+  Full,
+  trace,
+  debug,
+  information,
+  warning,
+  error,
+  critical,
+  Silent,
+};
+class Logger final {
+private:
+  LogLevel            log_level;
+  const Logger *const parent_logger;
+  void
+  do_log(LogLevel level, std::vector<std::string> &chain, std::string &&time, std::string &&content) const;
+  Logger();
+  Logger(const Logger *const parent, std::string_view name);
+
+public:
+  const std::string name;
+  Logger(const Logger &)            = delete;
+  Logger(Logger &&)                 = delete;
+  Logger &operator=(const Logger &) = delete;
+  Logger &operator=(Logger &&)      = delete;
+
+  static Logger &get_global_logger();
+
+  template <typename... Elements>
+  inline void
+  log(LogLevel level, std::format_string<Elements...> format_string, Elements &&...elements) const {
+    std::ostringstream buffer;
+    auto               time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+    buffer << std::put_time(std::localtime(&time), "|%F|%T|");
+    std::vector<std::string> chain;
+    return this->do_log(
+      level, chain, buffer.str(), std::format(format_string, std::forward<Elements>(elements)...)
+    );
+  }
+#define LOG_LEVEL_ENTRY(level)                                                                               \
+  template <typename... Elements>                                                                            \
+  inline void level(std::format_string<Elements...> format_string, Elements &&...elements) const {           \
+    return this->log(LogLevel::level, format_string, std::forward<Elements>(elements)...);                   \
+  }
+  LOG_LEVEL_ENTRY(trace);
+  LOG_LEVEL_ENTRY(debug);
+  LOG_LEVEL_ENTRY(information);
+  LOG_LEVEL_ENTRY(warning);
+  LOG_LEVEL_ENTRY(error);
+  LOG_LEVEL_ENTRY(critical);
+
+  [[nodiscard]] Logger create_sub_logger(std::string_view name) const;
+  void                 set_log_level(LogLevel new_level);
+};
+Logger &logger = Logger::get_global_logger();
+} // namespace LogPP
+#endif
